0ef6e3d765d7d6c5eafeaebcbcfb5d3c7896fe18
brucem90 <brucem90@gmail.com>
2013-02-07 22:39:20 -0800

    homework3 submission

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -23,13 +23,24 @@ Background: movies have been added to database
   
 Scenario: restrict to movies with 'PG' or 'R' ratings
   # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
+#  When I check the "PG" checkbox
+#    And I check "R"
+  Given I check the following ratings: PG, R
+    # enter step(s) to uncheck all other checkboxes
+    And I uncheck the following ratings: G, PG-13
+    # enter step to "submit" the search form on the homepage
+    And I press "ratings_submit"
   # enter step(s) to ensure that PG and R movies are visible
+  Then I should see "The Incredibles"
+    And I should see "The Terminator"
   # enter step(s) to ensure that other movies are not visible
+    And I should not see "Chocolat"
+    And I should not see "Aladdin"
 
 Scenario: no ratings selected
   # see assignment
 
 Scenario: all ratings selected
   # see assignment
+  Given I check all the ratings
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+  Movie.create(:title => movie["title"], :rating => movie["rating"], :release_date => movie["release_date"])
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,7 +14,19 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+  pos_e1 = page.body.index(e1)
+  pos_e2 = page.body.index(e2)
+  if pos_e1 < pos_e2 then before = true else before = nil end
+  before.should == true
+end
+
+Then /I should not see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  pos_e1 = page.body.index(e1)
+  pos_e2 = page.body.index(e2)
+  if pos_e1 > pos_e2 then after = true else after = nil end
+  after.should == true
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,4 +37,43 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings = rating_list.split(', ')
+  if !uncheck
+    ratings.each do |rating|
+      check('ratings_'+rating) #step "When I check '#{rating}'"
+    end
+  else
+    ratings.each do |rating|
+      uncheck('ratings_'+rating)
+    end
+  end
 end
+
+When /I (un)?check all the ratings/ do |uncheck|
+  ratings = ['G','PG','PG-13','R']
+  if !uncheck
+    ratings.each do |rating|
+      check('ratings_'+rating)
+    end
+  else
+    ratings.each do |rating|
+      uncheck('ratings_'+rating)
+    end
+  end
+end
+
+Then /^(?:|I )should see all of the movies/ do
+  value = Movie.count
+  rows = page.all('#movies tbody/tr').size
+  rows.should == value
+end
+# When /^(?:|I )press "([^"]*)"$/ do |button|
+#  click_button(button)
+# end
+
+
+
+# Then /^(?:|I )should see "([^"]*)" before "([^"]*)"$/ do |str1, str2|
+#  match = /#{str1}.*#{str2}/m =~ page.body
+# end
+
